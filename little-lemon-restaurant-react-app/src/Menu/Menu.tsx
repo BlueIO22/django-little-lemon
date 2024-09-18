adb2d549a389f70a216bca36889341a41c34a263
Sørenes <marius.sorenes@capgemini.com>
2024-09-18 10:53:35 +0200

    did some organizing of not required code#

--- little-lemon-restaurant-react-app/src/Menu/Menu.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { getMenuItems } from "../api/menuItems";
-
-export default function Menu() {
-  useEffect(() => {
-    getMenuItems().then((data) => {});
-  }, []);
-  return <></>;
-}
--- little-lemon-restaurant-react-app/src/api/menuItems.ts
+++
@@ -1,9 +0,0 @@
-export async function getMenuItems() {
-  const response = await fetch("http://localhost:8000/api/menu-items", {
-    method: "GET",
-    headers: {
-      contentType: "application/json",
-    },
-  });
-  console.log(response);
-}
